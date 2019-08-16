a1851c68905974267d64fc0fa260d271a64941c0
Greg Herbowicz <herbowicz@gmail.com>
2019-08-16 19:44:14 +0200

    my server config

--- deploy/shipitfile.js
+++ deploy/shipitfile.js
@@ -1,42 +1,42 @@
 // shipitfile.js
 module.exports = shipit => {
   // Load shipit-deploy tasks
-  require('shipit-deploy')(shipit)
-  require('shipit-shared')(shipit)
+  require("shipit-deploy")(shipit);
+  require("shipit-shared")(shipit);
 
   shipit.initConfig({
     default: {
-      deployTo: '/var/www/ep9',
-      repositoryUrl: 'git@github.com:JSCasts-episodes/ep9-deploy-via-shipit.git',
+      deployTo: "/var/www/ep9",
+      repositoryUrl: "https://github.com/herbowicz/ep9-deploy-via-shipit.git",
       shared: {
-        dirs: ['node_modules'],
-        overwrite: true,
-      },
+        dirs: ["node_modules"],
+        overwrite: true
+      }
     },
     staging: {
-      servers: 'root@68.183.219.150',
-    },
-  })
+      servers: "root@80.211.3.196"
+    }
+  });
 
-  shipit.blTask('npm:install', async () => {
-    await shipit.remote(`cd ${shipit.releasePath} && npm install --production`)
-  })
+  shipit.blTask("npm:install", async () => {
+    await shipit.remote(`cd ${shipit.releasePath} && npm install --production`);
+  });
 
-  shipit.blTask('server:start', async () => {
-    const command = 'NODE_ENV=production forever start current/server.js'
-    await shipit.remote(`cd ${shipit.config.deployTo} && ${command}`)
-  })
+  shipit.blTask("server:start", async () => {
+    const command = "NODE_ENV=production forever start current/server.js";
+    await shipit.remote(`cd ${shipit.config.deployTo} && ${command}`);
+  });
 
-  shipit.blTask('server:restart', async () => {
-    const command = 'forever restartall'
-    await shipit.remote(`cd ${shipit.config.deployTo} && ${command}`)
-  })
+  shipit.blTask("server:restart", async () => {
+    const command = "forever restartall";
+    await shipit.remote(`cd ${shipit.config.deployTo} && ${command}`);
+  });
 
-  shipit.on('updated', () => {
-    shipit.start('npm:install')
-  })
+  shipit.on("updated", () => {
+    shipit.start("npm:install");
+  });
 
-  shipit.on('published', () => {
-    shipit.start('server:restart')
-  })
-}
+  shipit.on("published", () => {
+    shipit.start("server:restart");
+  });
+};
